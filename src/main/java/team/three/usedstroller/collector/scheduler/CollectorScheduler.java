91eac40dc8b86855038ee9e9eb171824d3cad381
hoonyhoney <hoonyhoeny@gmail.com>
2024-12-09 23:36:44 +0900

    feat: 설정정보 수정, 새벽 1시 수집

--- src/main/java/team/three/usedstroller/collector/scheduler/CollectorScheduler.java
+++ src/main/java/team/three/usedstroller/collector/scheduler/CollectorScheduler.java
@@ -26,38 +26,38 @@ public class CollectorScheduler {
   private final SecondWearServiceMvc secondWearService;
   private final CommonService commonService;
 
-  @Scheduled(cron = "0 0 3 * * *", zone = "Asia/Seoul")
-  public void bunjang() {
-    log.info("번개장터 예약 수집 시작");
-    bunJangService.start();
-  }
+//  @Scheduled(cron = "0 0 3 * * *", zone = "Asia/Seoul")
+//  public void bunjang() {
+//    log.info("번개장터 예약 수집 시작");
+//    bunJangService.start();
+//  }
+//
+//  @Scheduled(cron = "0 5 3 * * *", zone = "Asia/Seoul")
+//  public void secondwear() {
+//    log.info("세컨웨어(구 헬로마켓) 예약 수집 시작");
+//    secondWearService.start();
+//  }
+//
+//  @Scheduled(cron = "0 10 3 * * *", zone = "Asia/Seoul")
+//  public void junggonara() {
+//    log.info("중고나라 예약 수집 시작");
+//    junggonaraService.start();
+//  }
+//
+//  @Scheduled(cron = "0 15 3 * * *", zone = "Asia/Seoul")
+//  public void naver() {
+//    log.info("네이버쇼핑 예약 수집 시작");
+//    naverService.start();
+//  }
+//
+//  @Scheduled(cron = "0 20 3 * * *", zone = "Asia/Seoul")
+//  public void carrot() {
+//    log.info("당근 예약 수집 시작");
+//    carrotService.start();
+//    commonService.updateModel();
+//  }
 
-  @Scheduled(cron = "0 5 3 * * *", zone = "Asia/Seoul")
-  public void secondwear() {
-    log.info("세컨웨어(구 헬로마켓) 예약 수집 시작");
-    secondWearService.start();
-  }
-
-  @Scheduled(cron = "0 10 3 * * *", zone = "Asia/Seoul")
-  public void junggonara() {
-    log.info("중고나라 예약 수집 시작");
-    junggonaraService.start();
-  }
-
-  @Scheduled(cron = "0 15 3 * * *", zone = "Asia/Seoul")
-  public void naver() {
-    log.info("네이버쇼핑 예약 수집 시작");
-    naverService.start();
-  }
-
-  @Scheduled(cron = "0 20 3 * * *", zone = "Asia/Seoul")
-  public void carrot() {
-    log.info("당근 예약 수집 시작");
-    carrotService.start();
-    commonService.updateModel();
-  }
-
-  @Scheduled(cron = "0 30 15 * * *", zone = "Asia/Seoul")
+  @Scheduled(cron = "0 0 1 * * *", zone = "Asia/Seoul")
   public void collectAll() {
     log.info("collectAll start");
     carrotService.start();
